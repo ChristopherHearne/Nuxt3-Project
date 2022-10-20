5126c38244f3c37d7782ce978844a1ceb1897eb7
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-10-20 15:06:05 +0200

    added a watch to repos request in hopes of populating certain url endpoints

--- components/GitHubSection.vue
+++ components/GitHubSection.vue
@@ -1,71 +1,72 @@
 <template lang="">
-	<div class="repos-container">
-		<div v-for="repo in githubRepos" class="repo-item">
-			<div class="header-info">
-				<h2>{{repo.name}}</h2>
-				<span v-for="languages in getLanguagesForRepo(repo.owner.login, repo.name)">
-					{{languages}}
-				</span>
-			</div>
-			<h3>{{repo.description}}</h3>
-			<a :href="repo.html_url">Link to repo</a>
-			<p>{{`Starred by: ${repo.stargazers_count}`}}</p>
-		</div>
-	</div>
+  <div class="repos-container">
+    <div v-for="repo in githubRepos" class="repo-item">
+      <div class="header-info">
+        <h2>{{ repo.name }}</h2>
+      </div>
+	  <span v-for="lang in Object.keys(languages)">
+		{{lang}}
+	  </span>
+      <h3>{{ repo.description }}</h3>
+      <a :href="repo.html_url">Link to repo</a>
+      <p>{{ `Starred by: ${repo.stargazers_count}` }}</p>
+    </div>
+  </div>
 </template>
+
 <script>
-export default{
-	name: 'GitHubSection',
-	props: {
-		githubRepos: []
-	},
-	methods: {
-		async getLanguagesForRepo(userName, repoName){
-			const results = await fetch(`https://api.github.com/repos/${userName}/${repoName}/languages`)
-			const languages = await results.json()
-			return Object.keys(languages)
-		}
-	}
-}
+
+export default {
+  name: "GitHubSection",
+  props: {
+    githubRepos: [],
+	languages: [], 
+  },
+};
 </script>
 <style>
-	.repos-container{
-		display: flex;
-		max-height: 100vh;
-		justify-content: center; 
-		flex-direction: column;
-		overflow: auto;
-	}
+.repos-container {
+  display: flex;
+  max-height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+  overflow: auto;
+}
 
-	.repo-item{
-		background-color: #1a1b21;
-		border-radius: 20px; 
-		padding: 20px;
-		margin-bottom: 20px; 
-	}
+.repo-item {
+  background-color: #1a1b21;
+  border-radius: 20px;
+  padding: 20px;
+  margin-bottom: 20px;
+}
 
-	.repo-item h2{
-		color: #fff;
-		letter-spacing: -0.05em; 
-		font-size: 35px; 
-		margin: 0; 
-	}
+.repo-item h2 {
+  color: #fff;
+  letter-spacing: -0.05em;
+  font-size: 35px;
+  margin: 0;
+}
 
-	.repo-item h3{
-		font-size: 15px;
-		color: #F3BF99;
-		letter-spacing: -0.08em; 
-	}
+.repo-item h3 {
+  font-size: 15px;
+  color: #f3bf99;
+  letter-spacing: -0.08em;
+}
 
-	.repo-item p{
-		font-size: 12px; 
-		letter-spacing: -0.08em; 
-		color: #F3BF99;
-	}
+.repo-item p {
+  font-size: 12px;
+  letter-spacing: -0.08em;
+  color: #f3bf99;
+}
 
-	.repo-item a{
-		cursor: pointer;
-		text-decoration: none;
-		color: #fff;
-	}
+.repo-item a {
+  cursor: pointer;
+  text-decoration: none;
+  color: #fff;
+}
+
+.repo-item span{
+	color: #fff; 
+	border-radius: 30px;
+}
 </style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -6,22 +6,64 @@
       <FooterSection :profile="profile" />
     </div>
     <div class="github-container">
-      <GitHubSection :githubRepos="githubRepos"/> 
+      <GitHubSection :githubRepos="gitHubRepos" :languages="languagesPerRepo" />
     </div>
   </div>
 </template>
 
-<script setup>
-  const id = 1
-  const {data: profile} = await useFetch(`http://localhost:11019/api/profileinfo/${id}`) // Figure out proxy for Nuxt3
-  const {data: githubRepos} = await useFetch('https://api.github.com/users/ChristopherHearne/repos')
+<script async setup>
+import { getAllDependencies } from 'vue-bundle-renderer/runtime';
+
+
+const getLanguages = async (req) => {
+  const {data: response} = await useFetch(req)
+  console.log(response)
+  return response
+};
+
+let languagesPerRepo = []
+
+const id = 1;
+
+const { data: profile } = await useFetch(
+  `http://localhost:11019/api/profileinfo/${id}`
+); // Figure out proxy for Nuxt3
+
+const { data: gitHubRepos } = await useFetch(
+  "https://api.github.com/users/ChristopherHearne/repos",
+  {
+    onResponse({ request, response }) {
+      return response._data;
+    },
+  }
+);
+
+watch(
+  gitHubRepos,
+  async (newVal) => {
+    if (newVal){
+      const data = JSON.parse(JSON.stringify(newVal));
+      let results = data.map((e) => useFetch(e.languages_url))
+      console.log(results)
+      const datas = await Promise.all(results)
+      languagesPerRepo = datas
+      console.log(languagesPerRepo)
+    }
+  },
+  {
+    deep: true,
+    immediate: true
+  }
+);
+
+watch;
 </script>
 <style>
 .profile-body {
   margin: 0;
   box-sizing: border-box;
   background-color: #23252c;
-  display: flex; 
+  display: flex;
   justify-content: space-evenly;
   align-items: center;
   font-family: "Inter", sans-serif;
@@ -30,11 +72,10 @@
   padding: 50px;
 }
 
-.info-container{
+.info-container {
   margin-left: 50px;
-  margin-bottom: 100px; 
+  margin-bottom: 100px;
 }
-
 
 body {
   margin: 0;
