be5b3957b956460b1fa3f0f069540f1d2cf4ea0d
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-10-26 13:08:05 +0200

    put all inputs into a form and changed to relevant types

--- components/FormSection.vue
+++ components/FormSection.vue
@@ -1,69 +1,61 @@
 <template lang="">
   <div class="input-container">
-    <span>Firstname</span>
-    <input
-      placeholder="Firstname..."
-      type="text"
-      v-model="profileInfo.firstName"
-    />
-    <span>Lastname</span>
-    <input
-      placeholder="Lastname..."
-      type="text"
-      v-model="profileInfo.lastName"
-    />
-    <span>Title</span>
-    <input placeholder="Title..." type="text" v-model="profileInfo.title" />
-    <span>Email</span>
-    <input placeholder="Email..." type="email" v-model="profileInfo.email" />
-    <span>About you</span>
-    <input placeholder="About..." type="text" v-model="profileInfo.about" />
-    <span> Your interests</span>
-    <input
-      placeholder="Interests..."
-      type="text"
-      v-model="profileInfo.interests"
-    />
-    <span> Avatar </span>
-    <input placeholder="Avatar" type="text" v-model="profileInfo.avatar" />
-    <span>Connect your Github Account</span>
+    <form @submit-prevent="handleSubmit" class="input-container">
+      <input
+        placeholder="Firstname..."
+        type="text"
+        v-model="profileInfo.firstName"
+      />
+      <input
+        placeholder="Lastname..."
+        type="text"
+        v-model="profileInfo.lastName"
+      />
+      <input placeholder="Title..." type="text" v-model="profileInfo.title" />
+      <input placeholder="Email..." type="email" v-model="profileInfo.email" />
+      <input placeholder="About..." type="text" v-model="profileInfo.about" />
+      <textarea
+        placeholder="Tell us something about you..."
+        v-model="profileInfo.about"
+      ></textarea>
+      <textarea
+        placeholder="Tell us about your interests..."
+        v-model="profileInfo.interests"
+      ></textarea>
+      <input placeholder="Avatar" type="file" @change="profileInfo.avatar" />
+      <input
+        placeholder="Link to Facebook..."
+        type="url"
+        v-model="profileInfo.facebook"
+      />
+      <input
+        placeholder="Link to Instagram..."
+        type="url"
+        v-model="profileInfo.instagram"
+      />
+      <input
+        placeholder="Link to Github..."
+        type="url"
+        v-model="profileInfo.github"
+      />
+      <input
+        placeholder="Link to LinkedIn..."
+        type="url"
+        v-model="profileInfo.linkedin"
+      />
+      <input
+        placeholder="Link to website/project..."
+        type="url"
+        v-model="profileInfo.website"
+      />
+      <button type="submit">Submit</button>
+      <button>Clear</button>
+    </form>
     <div class="github-container">
       <input placeholder="Your Github Username" v-model="gitHubUser" />
       <button @click="getRepos">Check for repos</button>
       <div class="github-message"></div>
     </div>
-    <span>Your Facebook</span>
-    <input
-      placeholder="Link to Facebook..."
-      type="url"
-      v-model="profileInfo.facebook"
-    />
-    <span>Your Instagram</span>
-    <input
-      placeholder="Link to Instagram..."
-      type="url"
-      v-model="profileInfo.instagram"
-    />
-    <span>Your Github</span>
-    <input
-      placeholder="Link to Github..."
-      type="url"
-      v-model="profileInfo.github"
-    />
-    <span>Your LinkedIn</span>
-    <input
-      placeholder="Link to LinkedIn..."
-      type="url"
-      v-model="profileInfo.linkedin"
-    />
-    <span>Your website</span>
-    <input
-      placeholder="Link to website/project..."
-      type="url"
-      v-model="profileInfo.website"
-    />
-    <button @click="postProfile">Submit</button>
-    <button>Clear</button>
   </div>
 </template>
 <script>
@@ -77,17 +69,20 @@ export default {
     };
   },
   methods: {
-    async postProfile() {
+    async handleSubmit() {
       const profileData = { ...this.profileInfo };
+
       var form_data = new FormData();
+
       Object.keys(profileData).forEach((key) =>
         form_data.append(key, profileData[key])
       );
+
       return await fetch("http://localhost:10157/api/Profile", {
         method: "POST",
         mode: "no-cors",
         body: form_data,
-      })
+      });
     },
     async getRepos() {
       const response = await fetch(
@@ -130,5 +125,14 @@ export default {
   font-family: "Inter", sans-serif;
   padding: 8px;
   width: 190px;
+}
+
+textarea{
+	font-family: 'Inter', sans-serif;
+	padding: 15px; 
+}
+
+button{
+	cursor: pointer; 
 }
 </style>
