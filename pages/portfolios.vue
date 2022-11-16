e53478dc6938fab013f47085eb86f5d4cd1000c7
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-11-16 11:50:29 +0100

    styling to chevron arrow, animates on hover and shows content

--- pages/portfolios.vue
+++ pages/portfolios.vue
@@ -2,42 +2,58 @@
   <div class="portfolios-container">
     <div
       class="portfolios-item"
-      v-for="profile in profiles"
-      @click="goToPortfolio(profile.profileName)">
-      <div class="item-header" :style="hover ? 'display: none;' : ''" >
-        <h2>{{ profile.firstName + " " + profile.lastName }}</h2>
-        <div class="item-about">
-          <span>{{profile.title}}</span>
+      v-for="(profile, index) in profiles"
+      :key="profile.id"
+      @click="goToPortfolio(profile.profileName)"
+    >
+      <div class="item-header">
+        <div class="item-header-default">
+          <h2>{{ profile.firstName + " " + profile.lastName }}</h2>
+          <div class="item-about">
+            <span>{{ profile.title }}</span>
+          </div>
+          <div class="icons--container">
+            <a :href="profile.facebook" class="facebook--icon" target="_blank"
+              ><i class="fa-brands fa-facebook-f"></i
+            ></a>
+            <a :href="profile.instagram" class="instagram--icon" target="_blank"
+              ><i class="fa-brands fa-instagram"></i
+            ></a>
+            <a :href="profile.linkedin" class="linkedin--icon" target="_blank"
+              ><i class="fa-brands fa-linkedin"></i
+            ></a>
+            <a :href="profile.github" class="github--icon" target="_blank"
+              ><i class="fa-brands fa-github"></i
+            ></a>
+          </div>
         </div>
-        <div class="icons--container">
-          <a :href="profile.facebook" class="facebook--icon" target="_blank"
-            ><i class="fa-brands fa-facebook-f"></i
-          ></a>
-          <a :href="profile.instagram" class="instagram--icon" target="_blank"
-            ><i class="fa-brands fa-instagram"></i
-          ></a>
-          <a :href="profile.linkedin" class="linkedin--icon" target="_blank"
-            ><i class="fa-brands fa-linkedin"></i
-          ></a>
-          <a :href="profile.github" class="github--icon" target="_blank"
-            ><i class="fa-brands fa-github"></i
-          ></a>
+        <div class="item-header-hover" v-if="hover && currentIndex === index">
+          Frontend
         </div>
       </div>
-      <div class="item-avatar" :style="hover ? 'display: none;' : ''">
-        <img :src="profile.avatar" class="avatar">
+      <div class="item-avatar">
+        <img class="avatar" :src="profile.avatar" />
+      </div>
+      <div
+        class="item-chevron"
+        @mouseover="
+          hover = true;
+          currentIndex = index;
+        "
+        @mouseout="
+          hover = false;
+          currentIndex = -1;
+        "
+      >
         <i class="fa-solid fa-chevron-right"></i>
-      </div>
-      <div class="item-dev-info" v-if="hover">
-        Frontend
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const app = useNuxtApp()
-const profiles = await app.$profileRepository.getAuthenticatedProfiles()
+const app = useNuxtApp();
+const profiles = await app.$profileRepository.getAuthenticatedProfiles();
 
 const goToPortfolio = (profileName) => {
   navigateTo(`/browse/${profileName}`);
@@ -45,12 +61,14 @@ const goToPortfolio = (profileName) => {
 </script>
 <script>
 export default {
-  data(){
+  data() {
     return {
-      hover: false
-    }
-  }
-}
+      currentIndex: -1,
+      hover: false,
+    };
+  },
+  computed() {},
+};
 </script>
 <style scoped>
 .portfolios-container {
@@ -60,13 +78,13 @@ export default {
   grid-template-columns: repeat(4, 1fr);
   row-gap: 2em;
   column-gap: 1em;
-  margin: 0 2em 0 9%; 
-  padding: 50px; 
+  margin: 0 2em 0 9%;
+  padding: 50px;
 }
 
 .portfolios-item {
-	height: auto; 
-	width: 100%;
+  height: auto;
+  width: 100%;
   cursor: pointer;
   display: flex;
   flex-direction: row;
@@ -77,67 +95,75 @@ export default {
   border-radius: 20px;
   text-align: left;
   margin: 0 auto;
-  padding: 4.5rem 1.5rem 4rem 1.5rem; 
+  padding: 4.5rem 1.5rem 4rem 1.5rem;
   overflow: visible;
-  transition: transform .8s ease; 
+  transition: transform 0.8s ease;
 }
 
-.portfolios-item:hover{
+.portfolios-item:hover {
   transform: scale(1.02);
   filter: brightness(1.05);
 }
 
-.item--header{
-	display: flex;
-	justify-content: space-between;
+.item--header {
+  display: flex;
+  justify-content: space-between;
   flex-direction: column;
   align-items: baseline;
-  font-weight: 400; 
+  font-weight: 400;
 }
 
 .icons--container {
   display: flex;
-  padding: .4em 1em 1em 0; 
+  padding: 0.4em 1em 1em 0;
   justify-content: left;
   align-items: center;
-  color: #fff; 
+  color: #fff;
 }
-div a i{
+div a i {
   color: #918e9b;
-  margin-right: 1em; 
+  margin-right: 1em;
 }
-h2{
-  margin: 10px 0 10px 0; 
+h2 {
+  margin: 10px 0 10px 0;
   font-weight: 300;
   letter-spacing: -0.02em;
-  width: 250px;   
+  width: 250px;
 }
 
-.item-about{
+.item-about {
   margin: 10px 0 5px 0;
 }
 
-.avatar{
+.avatar {
   border-radius: 50%;
-  width: 4.5em; 
+  width: 4.5em;
   height: auto;
 }
 
-.item-avatar{
+.item-avatar {
   width: 100%;
   height: auto;
-  display: flex; 
+  display: flex;
   align-items: center;
   justify-content: space-evenly;
 }
 
-span{
-  color: #F3BF99;
+span {
+  color: #f3bf99;
 }
 
-.fa-chevron-right{
+.fa-chevron-right {
   margin-left: 40px;
-  color: #918e9b; 
-  font-size: 25px 
+  color: #918e9b;
+  font-size: 25px;
+  transition: transform 0.3s ease 0s;
+}
+
+.fa-chevron-right:hover {
+  transform: rotate(180deg);
+}
+.hide {
+  display: none;
 }
 </style>
