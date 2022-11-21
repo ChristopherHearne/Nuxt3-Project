f9d8682a73b61b2ecb3a075d24b26a1c0db510e9
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-11-21 08:28:21 +0100

    removed redundant todo

--- backend/Controllers/TokenController.cs
+++ backend/Controllers/TokenController.cs
@@ -23,7 +23,7 @@ namespace API_Test.Controllers
             Configuration = config; 
         }
 
-        // TODO: We're getting what we need here, which is to post our access token t oour specific user. But we need to use the API to simply return the results. Use the other endpoints to post access tokens for specific users 
+        
         [HttpGet("/github/oauth/generate/token")]
         [Consumes("application/json")]
         [Produces("application/json")]
