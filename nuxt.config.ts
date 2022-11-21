0983addb4445498473dbbf049bad0f50a8d425c8
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-11-21 10:47:27 +0100

    created a fontawesome plugin for importing icons, now works when building for prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
 	  // Global CSS: https://go.nuxtjs.dev/config-css
-	  css: ["~/assets/main.css", "~/assets/animations.css"],
+	  css: ["~/assets/main.css", "~/assets/animations.css", "@fortawesome/fontawesome-free/css/all.min.css"],
 	
 	  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	  plugins: [
