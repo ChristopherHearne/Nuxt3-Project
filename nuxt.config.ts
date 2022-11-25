fbcd5dedd9070648d554b48ded94000218044622
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-11-25 14:58:26 +0100

    added vuetify library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
 	  // Global CSS: https://go.nuxtjs.dev/config-css
-	  css: ["~/assets/main.css", "~/assets/animations.css", "@fortawesome/fontawesome-free/css/all.min.css"],
+	  css: ["~/assets/main.css", "~/assets/animations.css", "@fortawesome/fontawesome-free/css/all.min.css", 'vuetify/lib/styles/main.sass'],
 	
 	  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	  plugins: [
@@ -29,5 +29,6 @@ export default defineNuxtConfig({
 	
 	  // Build Configuration: https://go.nuxtjs.dev/config-build
 	  build: {
+		transpile: ['vuetify']
 	  }, 
 })
