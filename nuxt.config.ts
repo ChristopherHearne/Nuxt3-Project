352a7100678338defb3b60294cebcb7e72cf1d80
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-12-15 15:03:20 +0100

    added custom loading page component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
 	  },
 	  // Auto import components: https://go.nuxtjs.dev/config-components
 	  components: true,
+
 	
 	  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 	  buildModules: [],
