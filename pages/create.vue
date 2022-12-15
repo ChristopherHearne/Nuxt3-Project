f781cff860f0b72ea007f0a00068c32df05c925a
Christopher Hearne <christopherhearne@Christophers-MacBook-Pro.local>
2022-12-15 12:10:29 +0100

    new profile creator implemented

--- pages/create.vue
+++ pages/create.vue
@@ -1,15 +1,72 @@
 <template>
 	<div class="form-container">
-		<FormSection />
+	  <div class="state">
+			  <ProfileCreatorStateNavigator />
+		  </div>
+	  <div class="inputs">
+		<component :store="profileStore" :is="stateComponents[navigator.currentNavstate]"></component>
+		<ProfileCreatorFooter />
+	  </div>
 	</div>
-</template>
+  </template>
+  
+  <script setup>
+  import { useNavigator } from '~~/stores/navigator';
+  import { useProfileData } from '~~/stores/profiledata';
+  const navigator = useNavigator()
+  const profileStore = useProfileData()
+  
+  const stateComponents = [
+	  'BasicInfoVue',
+	  'BioVue',
+	  'SocialLinksVue',
+	  'ProfilenameVue',
+	  'ConnectGitSectionVue'
+  ]
+  
+  </script>
+  <script>
+  import BasicInfoVue from '~~/components/ProfileCreator/BasicInfo.vue';
+  import BioVue from '~~/components/ProfileCreator/Bio.vue';
+  import SocialLinksVue from '~~/components/ProfileCreator/SocialLinks.vue';
+  import ProfilenameVue from '~~/components/ProfileCreator/Profilename.vue';
+  import ConnectGitSectionVue from '~~/components/ConnectGitSection.vue';
+  import { useProfileData } from '~~/stores/profiledata';
+  export default{
+	  components: {
+		  BasicInfoVue, BioVue, SocialLinksVue, ProfilenameVue, ConnectGitSectionVue
+	  }
+  }
+  </script>
+  <style scoped>
+  .form-container {
+	margin-left: 10%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	width: 90%;
+}
 
-<script setup>
-	
-</script>
+.state, .inputs{
+	animation: fadeInFromBottom 1s ease-in-out;
+  }
 
-<style scoped>
-	.form-container{
-		margin: 0 auto; 
-	}
-</style>
+  @keyframes fadeInFromBottom{
+  0%{
+    margin: 12em 0 6em 0;
+    opacity: 0;
+  }
+
+  1%{
+    margin: 12em 0 6em 0; 
+    opacity: 0;
+  }
+
+  100%{
+    margin: 6em 0 6em 0; 
+    opacity: 1; 
+  }
+}
+  </style>
+  
